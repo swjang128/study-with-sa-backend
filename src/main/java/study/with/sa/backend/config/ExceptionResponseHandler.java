5815b28a80c5c2d968d696ff506b1c759fa0d12b
swjang128 <101680109+swjang128@users.noreply.github.com>
2024-04-02 15:46:14 +0900

    로그 수정

--- src/main/java/study/with/sa/backend/config/ExceptionResponseHandler.java
+++ src/main/java/study/with/sa/backend/config/ExceptionResponseHandler.java
@@ -3,9 +3,8 @@ package study.with.sa.backend.config;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.sun.jdi.request.DuplicateRequestException;
 import jdk.jfr.Description;
+import lombok.extern.slf4j.Slf4j;
 import org.json.JSONException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
@@ -19,16 +18,15 @@ import java.util.concurrent.TimeoutException;
 
 @Description("Exception Handlers to customize message and result")
 @RestControllerAdvice
+@Slf4j
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class ExceptionResponseHandler extends ResponseEntityExceptionHandler {
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
     @Description("Request bad request exception abort")
     @ExceptionHandler({HttpClientErrorException.BadRequest.class,
             HttpClientErrorException.NotFound.class,
             HttpClientErrorException.MethodNotAllowed.class})
     public ApiResponse handleBadRequestException(Exception e) {
-        logger.error("handle BadRequestException: {}", e.getMessage());
+        log.error("handle BadRequestException: {}", e.getMessage());
         return ApiResponse.error(HttpStatus.BAD_REQUEST, e.getMessage());
     }
 
@@ -37,21 +35,21 @@ public class ExceptionResponseHandler extends ResponseEntityExceptionHandler {
             JsonProcessingException.class, JSONException.class, IOException.class,
             TimeoutException.class, DuplicateRequestException.class})
     public ApiResponse handleException(Exception e) {
-        logger.error("handle Exception: {}", e.getMessage());
+        log.error("handle Exception: {}", e.getMessage());
         return ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
     @Description("Request unauthorized exception abort")
     @ExceptionHandler(HttpClientErrorException.Unauthorized.class)
     public ApiResponse handleUnauthorizedException(HttpClientErrorException e) {
-        logger.error("handle UnauthorizedException: {}", e.getMessage());
+        log.error("handle UnauthorizedException: {}", e.getMessage());
         return ApiResponse.error(HttpStatus.UNAUTHORIZED, e.getMessage());
     }
 
     @Description("Request forbidden exception abort")
     @ExceptionHandler({HttpClientErrorException.Forbidden.class, HttpClientErrorException.NotAcceptable.class})
     public ApiResponse handleForbiddenException(HttpClientErrorException e) {
-        logger.error("handle ForbiddenException: {}", e.getMessage());
+        log.error("handle ForbiddenException: {}", e.getMessage());
         return ApiResponse.error(HttpStatus.FORBIDDEN, e.getMessage());
     }
 }
